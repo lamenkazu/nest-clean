70e6592fc2396dc4f056d5f7d7f938003b502743
Lamenkazu <333.erick@gmail.com>
2024-03-19 11:28:07 -0300

    ⚙️ Configurando autenticação JWT

--- commit-notes.md
+++ commit-notes.md
@@ -1,2 +1,4 @@
 Neste commit foi configurado o projeto para acessar as variáveis de ambiente em qualquer arquivo de aplicação.
 Foi utilizado o @nestjs/config do Nest, que é recomendado pela documentação.
+
+Neste commit, começa a configuração da autenticação JWT na aplicação, utilizando Passport que é uma biblioteca popular para autenticação no Node.js, e o JWT é uma estratégia de autenticação escolhida para a aplicação
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { CreateAccountController } from "./controllers/create-account.controller
 
 import { ConfigModule } from "@nestjs/config";
 import { envSchema } from "./env";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { envSchema } from "./env";
       validate: (env) => envSchema.parse(env),
       isGlobal: true, // Para não configurar em todos os módulos da aplicação.
     }),
+    AuthModule,
   ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
