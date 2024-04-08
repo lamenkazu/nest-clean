764670fabbf6019c149f45671d1b67145f0df087
Lamenkazu <333.erick@gmail.com>
2024-04-08 20:35:45 -0300

    ✨ Redis Cache Service

---
+++ src/infra/cache/redis/redis.service.ts
@@ -0,0 +1,17 @@
+import { EnvService } from "@/infra/env/env.service";
+import { OnModuleDestroy } from "@nestjs/common";
+import { Redis } from "ioredis";
+
+export class RedisService extends Redis implements OnModuleDestroy {
+  constructor(private envService: EnvService) {
+    super({
+      host: envService.get("REDIS_HOST"), //localhost
+      port: envService.get("REDIS_PORT"),
+      db: envService.get("REDIS_DB"),
+    });
+  }
+
+  onModuleDestroy() {
+    return this.disconnect();
+  }
+}
